061e3057c552708583c6f92e17bbdff55682e56e
nothing-special20 <rquin@billmoretech.com>
2024-01-28 16:40:39 -0500

    add zoho endpoint calls

--- zoho/crm/main.py
+++ zoho/crm/main.py
@@ -16,6 +16,7 @@ ZOHO_TEMP_CODE = config["ZOHO_TEMP_CODE"]
 ZOHO_CRM_OWNER_ID = config["ZOHO_CRM_OWNER_ID"]
 ZOHO_CRM_OWNER_NAME = config["ZOHO_CRM_OWNER_NAME"]
 ZOHO_AUTH_TOKEN = config["ZOHO_AUTH_TOKEN"]
+ZOHO_REFRESH_TOKEN = config["ZOHO_REFRESH_TOKEN"]
 
 
 zoho_accounts_url = "https://accounts.zoho.com"
@@ -37,8 +38,23 @@ def authorization_token():
 
     return response.json()
 
+def refresh_authorization_token():
+    url = "https://accounts.zoho.com/oauth/v2/token"
+
+    params = {
+        "refresh_token": ZOHO_REFRESH_TOKEN,
+        "client_id": ZOHO_CLIENT_ID,
+        "client_secret": ZOHO_CLIENT_SECRET,
+        "grant_type": "refresh_token"
+    }
+
+    response = requests.post(url=url, params=params)
+
+    return response.json()
+    
+
 #####Leads
-def create_lead(auth_token, records):
+def create_leads(auth_token, records):
     api_headers = {
         "Authorization": "Zoho-oauthtoken " + auth_token,
     }
@@ -57,6 +73,47 @@ def create_lead(auth_token, records):
 
     return response.json()
 
+#####Contacts
+def create_contacts(auth_token, records):
+    api_headers = {
+        "Authorization": "Zoho-oauthtoken " + auth_token,
+    }
+
+    for rec in records:
+        rec["Owner"] = {"id": ZOHO_CRM_OWNER_ID, "full_name": ZOHO_CRM_OWNER_NAME}
+
+    leads_url = "https://www.zohoapis.com/crm/v2.2/Contacts"
+    request_body = {
+        "data": records,
+    }
+
+    request_body = json.dumps(request_body)
+
+    response = requests.post(url=leads_url, headers=api_headers, data=request_body)
+
+    return response.json()
+
+def get_records(auth_token, module, id):
+    api_headers = {
+        "Authorization": "Zoho-oauthtoken " + auth_token,
+    }
+
+    module_url = f"https://www.zohoapis.com/crm/v2.2/{module}" #/{id}
+
+    response = requests.get(url=module_url, headers=api_headers)
+
+    return response
+
+def get_modules(auth_token):
+    api_headers = {
+        "Authorization": "Zoho-oauthtoken " + auth_token,
+    }
+
+    url = "https://www.zohoapis.com/crm/v2/settings/modules"
+
+    response = requests.get(url=url, headers=api_headers)
+
+    return response
 
 #####Deals
 def create_deal(auth_token, records):
@@ -67,7 +124,7 @@ def create_deal(auth_token, records):
     for rec in records:
         rec["Owner"] = {"id": ZOHO_CRM_OWNER_ID, "full_name": ZOHO_CRM_OWNER_NAME}
 
-    leads_url = "https://www.zohoapis.com/crm/v2.2/Leads"
+    leads_url = "https://www.zohoapis.com/crm/v2.2/Deals"
     request_body = {
         "data": records,
     }
@@ -86,7 +143,10 @@ if __name__ == "__main__":
         auth_token = auth_token_json["access_token"]
         print(auth_token)
 
-    elif sys.argv[1] == "create_lead":
+    elif sys.argv[1] == "refresh_authorization_token":
+        print(refresh_authorization_token())
+
+    elif sys.argv[1] == "create_leads":
         records = [
             {
                 "Email_Opt_Out": False,
@@ -104,9 +164,31 @@ if __name__ == "__main__":
             }
         ]
 
-        lead = create_lead(ZOHO_AUTH_TOKEN, records)
+        lead = create_leads(ZOHO_AUTH_TOKEN, records)
 
         print(lead)
+
+    elif sys.argv[1] == "create_contacts":
+        records = [
+            {
+                "Email_Opt_Out": False,
+                "Rating": "-None-",
+                "Lead_Status": "-None-",
+                "Industry": "-None-",
+                "Lead_Source": "-None-",
+                "Company": "test",
+                "Last_Name": "me",
+                "First_Name": "delete",
+                "Designation": "test",
+                "Phone": "1231231234",
+                "Website": "test.com",
+                "Description": "test",
+            }
+        ]
+
+        contacts = create_contacts(ZOHO_AUTH_TOKEN, records)
+
+        print(contacts)
 
     elif sys.argv[1] == "create_deal":
         records = [
@@ -114,7 +196,7 @@ if __name__ == "__main__":
                 "Ownership": "-None-",
                 "Rating": "-None-",
                 "Industry": "-None-",
-                "Account_Type": "-None-",
+                "Account_Type": "New Business",
                 "Account_Name": "test account",
                 "$zia_owner_assignment": "owner_recommendation_unavailable",
                 "zia_suggested_users": {},
@@ -122,3 +204,54 @@ if __name__ == "__main__":
         ]
 
         create_deal(ZOHO_AUTH_TOKEN, records)
+
+
+    elif sys.argv[1] == 'meetings':
+        # https://www.zoho.com/meeting/api-integration/meeting-api/create-a-meeting.html
+    
+        zsoid = '6068214000000587001'
+
+        url = f'https://meeting.zoho.com/api/v2/{zsoid}/sessions.json'
+        
+
+        api_headers = {
+            "Authorization": "Zoho-oauthtoken " + ZOHO_AUTH_TOKEN,
+        }
+
+        data = {
+            "session": {
+                "topic": "Monthly Marketing Meeting",
+                "agenda": "Points to get noted during meeting.",
+                "presenter": 123456789,
+                "startTime": "Jun 19, 2020 07:00 PM",
+                "duration": 3600000,
+                "timezone": "Asia/Calcutta",
+                "participants": [
+                    {
+                        "email": "dummy@email.com",
+                        "type": "lead",
+                        "id": ""
+                    }
+                ]
+            }
+        }
+
+        response = requests.post(url, headers=api_headers, json=data)
+        print(response.json())
+
+    elif sys.argv[1] == 'get_meetings':
+        zsoid = "org840250224"
+        meeting_key = "6068214000000587001"
+        url = f"https://meeting.zoho.com/api/v2/{zsoid}/sessions/{meeting_key}.json"
+        api_headers = {
+            "Authorization": "Zoho-oauthtoken " + ZOHO_AUTH_TOKEN,
+        }
+
+        response = requests.get(url, headers=api_headers)
+        print(response.text)
+        print(response.status_code)
+
+    elif sys.argv[1] == 'get_modules':
+        x = get_modules(ZOHO_AUTH_TOKEN)
+        print(x.text)
+        print(x.status_code)
