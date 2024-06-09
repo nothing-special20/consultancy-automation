0dd09717a284fcb9daeef4c16480e57f5dcaaa62
nothing-special20 <rquin@billmoretech.com>
2024-06-09 13:21:55 -0400

    update apollo search

--- marketing/email/apollo_io/main.py
+++ marketing/email/apollo_io/main.py
@@ -15,6 +15,7 @@ env_path = os.path.dirname(os.path.realpath(__file__)) + "/.env"
 ENV_VARS = dotenv_values(env_path)
 
 APOLLO_API_KEY = ENV_VARS["APOLLO_API_KEY"]
+# APOLLO_API_KEY = ENV_VARS["APOLLO_API_KEY_SECOND"]
 APOLLO_API_BASE_URL = "https://api.apollo.io"
 APOLLO_HEADERS = {"Content-Type": "application/json", "Cache-Control": "no-cache"}
 APOLLO_FOLDER = ENV_VARS["APOLLO_FOLDER"]
@@ -22,6 +23,7 @@ APOLLO_FOLDER = ENV_VARS["APOLLO_FOLDER"]
 # Documentation
 # https://knowledge.apollo.io/hc/en-us/articles/4416173158541-Use-the-Apollo-REST-API
 # https://apolloio.github.io/apollo-api-docs/?shell#search-for-contacts
+
 
 def post_contacts(params):
     url = APOLLO_API_BASE_URL + "/v1/contacts"
@@ -32,12 +34,15 @@ def post_contacts(params):
     print(response.status_code)
     print(json.dumps(response.json(), indent=4))
 
+
 def search_contacts(params):
     url = APOLLO_API_BASE_URL + "/v1/contacts/search"
 
     params["api_key"] = APOLLO_API_KEY
 
-    response = requests.post(url, headers=APOLLO_HEADERS, data=json.dumps(params))
+    response = requests.post(
+        url, headers=APOLLO_HEADERS, data=json.dumps(params), timeout=10
+    )
 
     try:
         return response.json()
@@ -49,6 +54,24 @@ def search_contacts(params):
 
 def search_people(params):
     url = APOLLO_API_BASE_URL + "/v1/mixed_people/search"
+    params["api_key"] = APOLLO_API_KEY
+
+    response = requests.post(url, headers=APOLLO_HEADERS, data=json.dumps(params))
+
+    return response.json()
+
+
+def search_accounts(params):
+    url = APOLLO_API_BASE_URL + "/v1/accounts/search"
+    params["api_key"] = APOLLO_API_KEY
+
+    response = requests.post(url, headers=APOLLO_HEADERS, data=json.dumps(params))
+
+    return response.json()
+
+
+def search_companies(params):
+    url = APOLLO_API_BASE_URL + "/v1/mixed_companies/search"
     params["api_key"] = APOLLO_API_KEY
 
     response = requests.post(url, headers=APOLLO_HEADERS, data=json.dumps(params))
@@ -71,6 +94,7 @@ def get_lists_of_lists():
 
     response = requests.get(url, headers=APOLLO_HEADERS, params=params)
     return response.json()
+
 
 def saved_searches():
     url = APOLLO_API_BASE_URL + "/v1/labels/search"
@@ -118,6 +142,7 @@ def get_people_from_list(list_id, page_num=1):
 
     return response.json()
 
+
 def map_label_id_to_name(label_id, label_records):
     label_ids = label_id.split(";")
 
@@ -154,6 +179,7 @@ def apollo_contact_etl_zoho_lead(x, tags=None):
         "Record_Image": x.get("photo_url", None),
         "Lead_Owner": x.get("owner_id", None),
         "Lead_Status": None,  # Not available
+        "linkedin": x["account"].get("linkedin_url", None) if "account" in x else None,
         "Mobile": None,  # Not available
         "Most_Recent_Visit": None,  # Not available
         "No. of Employees": None,  # Not available
@@ -297,13 +323,28 @@ if __name__ == "__main__":
         # sort data by created_at
         saved_lists = sorted(saved_lists, key=lambda k: k["created_at"], reverse=True)
 
-        for x in saved_lists[:6]:
+        # print(saved_lists)
+
+        # for x in saved_lists:
+        #     print(x)
+
+        for x in saved_lists[:1]:
+            print(x["name"])
+            # for x in []:
             saved_list_df = []
-            file_name = APOLLO_FOLDER + "apollo-" + re.sub(":| ", "_",x["name"]).lower() + ".csv"
+            file_name = (
+                APOLLO_FOLDER
+                + "apollo-"
+                + re.sub(":| ", "_", x["name"]).lower()
+                + ".csv"
+            )
             saved_list_size = x["cached_count"]
             print("saved list name:\t", x["name"])
             print("saved list size:\t", saved_list_size)
-            for i in range(1, (math.ceil(saved_list_size / 25) + 1)):
+            print((math.ceil(saved_list_size / 25) + 1))
+
+            page_count = math.ceil(saved_list_size / 25)  # + 21
+            for i in range(1, page_count):
                 try:
                     params = {
                         "label_ids": [x["_id"]],
@@ -314,13 +355,26 @@ if __name__ == "__main__":
                     }
                     main_contact_data = search_contacts(params)
 
-                    contacts = main_contact_data["contacts"]
-                    contacts = pd.concat(
-                        [
-                            pd.DataFrame([apollo_contact_etl_zoho_lead(x)])
-                            for x in contacts
-                        ]
-                    )
+                    _contacts = main_contact_data["contacts"]
+
+                    contacts = []
+
+                    for contact in _contacts:
+                        try:
+                            temp = apollo_contact_etl_zoho_lead(contact)
+                            temp = pd.DataFrame([temp])
+                            contacts.append(temp)
+                        except:
+                            print(traceback.format_exc())
+
+                    contacts = pd.concat(contacts)
+
+                    # contacts = pd.concat(
+                    #     [
+                    #         pd.DataFrame([apollo_contact_etl_zoho_lead(x)])
+                    #         for x in contacts
+                    #     ]
+                    # )
                     print(contacts.shape)
                     saved_list_df.append(contacts)
                 except:
@@ -329,10 +383,266 @@ if __name__ == "__main__":
                 time.sleep(3)
 
             saved_list_df = pd.concat(saved_list_df)
+            print("saved_list_df - pre drop duplicate rows:\t", saved_list_df.shape[0])
             saved_list_df.drop_duplicates(inplace=True)
-            saved_list_df["Lead_Category"] = saved_list_df["Lead_Category_ID"].apply(lambda x: map_label_id_to_name(x, saved_lists))
+            saved_list_df["Lead_Category"] = saved_list_df["Lead_Category_ID"].apply(
+                lambda x: map_label_id_to_name(x, saved_lists)
+            )
             saved_list_df.to_csv(file_name, index=False)
-
 
             print("saved_list_df rows:\t", saved_list_df.shape[0])
             print("saved_list_size :\t", saved_list_size)
+            print("saved to file:\t", file_name)
+
+    if sys.argv[1] == "split_data_for_smartlead":
+        folder = "/Users/robquin/Documents/Professional/Entrepreneur/Bill More Tech/consultancy-automation-data/integrations/smartlead_ready/"
+        # file = folder + "Sales_Mgrs_-_USA_-_21-50_1714431430_x14273_OK_ONLY_MILLIONVERIFIER.COM.csv"
+        file = (
+            folder
+            + "apollo - Sales Managers - Using Hubspot - 11-20 Employees_OK_ONLY_MILLIONVERIFIER.COM.csv"
+        )
+
+        data = pd.read_csv(file)
+
+        def company_name_cleaner(x):
+            output = re.sub(
+                "Inc.|LLC|Corp.|Co.|Ltd.|Limited|Incorporated|Corporation", "", x
+            )
+            output = re.sub("  ", " ", output)
+            return output
+
+        data["Company"] = [company_name_cleaner(x) for x in data["Company"]]
+
+        # data["domain"] = data["email"].apply(lambda x: x.split("@")[1] if "@" in x else None)
+
+        # split df into 2
+        data1 = data.iloc[:500]
+        data2 = data.iloc[500:]
+        # data3 = data.iloc[2400:]
+
+        data1.to_csv(file.split(".csv")[0] + "_1.csv", index=False)
+        data2.to_csv(file.split(".csv")[0] + "_2.csv", index=False)
+        # data2.to_csv(file.split(".csv")[0] + "_3.csv", index=False)
+
+    if sys.argv[1] == "search_accounts":
+
+        def parse_account_data(item):
+            extracted_item = {
+                "id": item["id"],
+                "name": item["name"],
+                "website_url": item["website_url"],
+                "blog_url": item["blog_url"],
+                "angellist_url": item["angellist_url"],
+                "linkedin_url": item["linkedin_url"],
+                "twitter_url": item["twitter_url"],
+                "facebook_url": item["facebook_url"],
+                "phone": item["phone"],
+                "linkedin_uid": item["linkedin_uid"],
+                "founded_year": item["founded_year"],
+                "publicly_traded_symbol": item["publicly_traded_symbol"],
+                "publicly_traded_exchange": item["publicly_traded_exchange"],
+                "logo_url": item["logo_url"],
+                "crunchbase_url": item["crunchbase_url"],
+                "primary_domain": item["primary_domain"],
+                # 'sanitized_phone': item['sanitized_phone'],
+                "organization_raw_address": item["organization_raw_address"],
+                "organization_city": item["organization_city"],
+                "organization_street_address": item["organization_street_address"],
+                "organization_state": item["organization_state"],
+                "organization_country": item["organization_country"],
+                "organization_postal_code": item["organization_postal_code"],
+                "suggest_location_enrichment": item["suggest_location_enrichment"],
+                "domain": item["domain"],
+                "team_id": item["team_id"],
+                "organization_id": item["organization_id"],
+                "account_stage_id": item["account_stage_id"],
+                "source": item["source"],
+                "original_source": item["original_source"],
+                "creator_id": item["creator_id"],
+                "owner_id": item["owner_id"],
+                "created_at": item["created_at"],
+                "phone_status": item["phone_status"],
+                "hubspot_id": item["hubspot_id"],
+                "salesforce_id": item["salesforce_id"],
+                "crm_owner_id": item["crm_owner_id"],
+                "parent_account_id": item["parent_account_id"],
+                "existence_level": item["existence_level"],
+                "modality": item["modality"],
+                "crm_record_url": item["crm_record_url"],
+                "num_contacts": item["num_contacts"],
+                "last_activity_date": item["last_activity_date"],
+                "intent_strength": item["intent_strength"],
+                "show_intent": item["show_intent"],
+                "has_intent_signal_account": item["has_intent_signal_account"],
+                "intent_signal_account": item["intent_signal_account"],
+            }
+
+            return extracted_item
+
+        # print(x)
+
+        saved_lists = get_lists_of_lists()
+        # sort data by created_at
+        saved_lists = sorted(saved_lists, key=lambda k: k["created_at"], reverse=True)
+
+        latest_list = saved_lists[0]
+        latest_list_id = latest_list["_id"]
+        latest_list_name = latest_list["name"]
+        print(latest_list.keys())
+        print(json.dumps(latest_list, indent=4))
+
+        latest_list_num_of_pages = math.ceil(latest_list["cached_count"] / 25)
+
+        response_df_lists = []
+
+        for page in range(1, latest_list_num_of_pages + 1):
+            params = {
+                "label_ids": [latest_list_id],
+                "sort_ascending": False,
+                "page": page,
+            }
+            accounts = search_accounts(params)
+
+            temp = [pd.DataFrame([parse_account_data(x)]) for x in accounts["accounts"]]
+            temp = pd.concat(temp)
+
+            response_df_lists.append(temp)
+
+            response_df = pd.concat(response_df_lists)
+
+            file = APOLLO_FOLDER + "apollo_companies" + latest_list_name + ".csv"
+            response_df.to_csv(file, index=False)
+
+            print("scraped page:\t", page, "of", latest_list_num_of_pages)
+
+            time.sleep(3)
+
+    if sys.argv[1] == "smartlead_cleaning_detailed_analysis":
+        folder = "/Users/robquin/Documents/Professional/Entrepreneur/Bill More Tech/consultancy-automation-data/integrations/smartlead_ready/"
+        # file = folder + "Sales_Mgrs_-_USA_-_21-50_1714431430_x14273_OK_ONLY_MILLIONVERIFIER.COM.csv"
+        file = (
+            folder
+            + "apollo - Biz Dev - Marketing & Advertising - USA - 1-50_OK_ONLY_MILLIONVERIFIER.COM.csv"
+        )
+
+        data = pd.read_csv(file)
+
+        def company_name_cleaner(x):
+            output = str(x)
+            output = re.sub(
+                "Inc.|LLC|Corp.|Co.|Ltd.|Limited|Incorporated|Corporation", "", output
+            )
+            output = re.sub("  ", " ", output)
+            output = output.strip()
+            return output
+
+        try:
+            del data["email_status"]
+        except:
+            pass
+
+        # rename columns
+        data.rename(
+            columns={
+                "first_name": "First Name",
+                "last_name": "Last Name",
+                "email": "Email",
+                "organization_name": "Company Name",
+            },
+            inplace=True,
+        )
+
+        data["Company Name"] = [company_name_cleaner(x) for x in data["Company Name"]]
+
+        # ignore blank companies
+        data = data[data["Company Name"] != ""]
+        data = data[data["Company Name"].notnull()]
+        # data = data[data["title"] == "Business Development Manager"]
+
+        data1 = data.iloc[:1300]
+        data2 = data.iloc[1300:]
+
+        data1.to_csv(file.split(".csv")[0] + "_1.csv", index=False)
+        data2.to_csv(file.split(".csv")[0] + "_2.csv", index=False)
+
+    if sys.argv[1] == "search_companies":
+        agency_websites_file = "agency_websites.json"
+
+        agency_domains = []
+
+        with open(agency_websites_file, "r") as f:
+            for line in f:
+                data = line.strip()
+                if data.endswith(","):
+                    data = data[:-1]
+                agency = json.loads(data)
+                agency_domains.append(agency["company_domain"])
+
+        agency_domains = [re.sub("www\\.", "", x) for x in agency_domains]
+        print(len(agency_domains))
+        agency_domains = list(set(agency_domains))
+        print(len(agency_domains))
+
+        # split agency_domains into chunks of 10
+        chunk_size = 10
+        chunks = [
+            agency_domains[i : i + chunk_size]
+            for i in range(0, len(agency_domains), chunk_size)
+        ]
+
+        org_ids = []
+
+        for chunk in chunks:
+            params = {
+                "page": 1,
+                "per_page": 10,
+                "q_organization_domains": "\n".join(chunk),
+            }
+
+            x = search_companies(params)
+
+            temp_organization_ids = [x["id"] for x in x["organizations"]]
+
+            org_ids.extend(temp_organization_ids)
+
+            time.sleep(3)
+
+        print(org_ids)
+
+        org_id_url = ["&organizationIds[]=" + str(x) for x in org_ids]
+        org_id_url = "".join(org_id_url)
+
+        print(org_id_url)
+
+        # params = {
+        #     "page": 1,
+        #     "per_page": 10,
+        #     "q_organization_domains": "themetainbound.com"
+        # }
+
+        # x = search_companies(params)
+
+        # print(json.dumps(x, indent=4))
+
+    if sys.argv[1] == "test":
+        saved_lists = get_lists_of_lists()
+        # sort data by created_at
+        saved_lists = sorted(saved_lists, key=lambda k: k["created_at"], reverse=True)
+
+        latest_list = saved_lists[0]
+
+        print(latest_list)
+        print(latest_list.keys())
+
+        params = {
+            "label_ids": [latest_list["_id"]],
+            "sort_by_field": "contact_last_activity_date",
+            "sort_ascending": False,
+            "page": 1,
+            # "per_page": 25,
+        }
+
+        x = search_companies(params)
+
+        print(json.dumps(x, indent=4))
+        print(latest_list["_id"])
